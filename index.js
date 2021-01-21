6299757346bc6a95acbe14727b213915c4b55f67
spenser6131 <brinkman.spenser@gmail.com>
2021-01-20 16:33:34 -0800

    commit tree c0d09aa59a146306cfa35abc4f1ef4552800c00f

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
-// Write your code here
+class Breakfast {
+  constructor(food, drink) {
+    this.food = food;
+    this.drink = drink;
+  }
+}
+
+class Lunch {
+  constructor(salad, soup, drink) {
+    this.soup = soup;
+    this.drink = drink;
+    this.salad = salad;
+  }
+}
+
+class Dinner {
+  constructor(salad, soup, entree, dessert) {
+    this.soup = soup;
+    this.entree = entree;
+    this._dessert = dessert;
+    this.salad = salad;
+  }
+}
